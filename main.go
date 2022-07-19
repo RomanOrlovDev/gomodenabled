431096731326a061b8c637be3ffd3a07bb53db33
sprutex <roman.orlov.dev@gmail.com>
2022-07-19 23:22:24 +0300

    pbormanuuid added

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package modenabled
 
 import (
 	"github.com/RomanOrlovDev/gomodenabled/helper"
+	"github.com/pborman/uuid"
 	"log"
 )
 
@@ -9,4 +10,5 @@ func PrintCustomMessage() {
 	log.Println("this functionality with enabled module...")
 	log.Println("simple upgrade #1 of the module...")
 	helper.FuncToHelp()
+	log.Println("uuid added to the module. Here it is: ", uuid.ClockSequence())
 }
